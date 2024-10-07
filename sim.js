b3b6e41da992e94cd66deb0e9233aab88845413e
Giacomo Rossettini <rossegiac@gmail.com>
2024-10-07 14:24:15 +0200

    finish

--- sim.js
+++ sim.js
@@ -1,1 +1,31 @@
-console.log("funziona");
+var User = /** @class */ (function () {
+    function User(nome, cognome) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.credito = 0;
+        this.numeroChiamate = 0;
+    }
+    User.prototype.ricarica = function (importo) {
+        this.credito += importo;
+    };
+    User.prototype.chiamata = function (minuti) {
+        var costoChiamata = 0.20 * minuti;
+        if (this.credito >= costoChiamata) {
+            this.credito -= costoChiamata;
+            this.numeroChiamate += minuti;
+        }
+        else {
+            console.log("Non hai abbastanza soldi sfigato");
+        }
+    };
+    User.prototype.chiama404 = function () {
+        return this.credito;
+    };
+    User.prototype.getNumeroChiamata = function () {
+        return this.numeroChiamate;
+    };
+    User.prototype.azzeraChiamate = function () {
+        this.numeroChiamate = 0;
+    };
+    return User;
+}());
--- sim.ts
+++ sim.ts
@@ -1,1 +1,44 @@
-console.log("funziona");
+interface Smartphone {
+    credito: number;
+    numeroChiamate: number;
+}
+
+class User implements Smartphone {
+    credito: number;
+    numeroChiamate: number;
+    nome: string;
+    cognome: string;
+
+    constructor(nome: string, cognome: string) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.credito = 0;
+        this.numeroChiamate = 0;
+    }
+
+    ricarica(importo: number): void {
+        this.credito += importo;
+    }
+
+    chiamata(minuti: number) {
+        const costoChiamata = 0.20 * minuti;
+        if (this.credito >= costoChiamata) {
+            this.credito -= costoChiamata;
+            this.numeroChiamate += minuti;
+        } else {
+            console.log("Non hai abbastanza soldi sfigato");
+        }
+    }
+
+    chiama404(): number {
+        return this.credito;
+    }
+
+    getNumeroChiamata(): number {
+        return this.numeroChiamate;
+    }
+
+    azzeraChiamate(): void {
+        this.numeroChiamate = 0;
+    }
+}
